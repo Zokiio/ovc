3aaee850579d2d73800ba1580ca4fa774b222982
Zokiio <zoki@zottik.com>
2026-01-29 18:52:34 +0100

    fix: update STUN server URLs for improved WebRTC connectivity

--- web-client/js/config.js
+++ web-client/js/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
     // WebRTC configuration
     RTC_CONFIGURATION: {
         iceServers: [
-            { urls: 'stun:stun.l.google.com:19302' },
-            { urls: 'stun:stun1.l.google.com:19302' },
+            { urls: 'stun:stun.cloudflare.com:3478' },
+            { urls: 'stun:stun.cloudflare.com:53' },
         ]
     },
     
